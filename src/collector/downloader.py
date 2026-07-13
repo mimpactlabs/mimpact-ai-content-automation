46af0ad1f74ff8c7ac4d083486bdf0650466263a
mimpactlabs <mimpactmedia@gmail.com>
2026-07-13 22:02:23 +0700

    Add YouTube downloader class

--- src/collector/downloader.py
+++ src/collector/downloader.py
@@ -1,1 +1,17 @@
+from yt_dlp import YoutubeDL
 
+
+class Downloader:
+
+    def download(self, url: str, output_path: str = "data/downloads"):
+
+        options = {
+            "outtmpl": f"{output_path}/%(id)s.%(ext)s",
+            "format": "bestvideo+bestaudio/best",
+            "merge_output_format": "mp4",
+        }
+
+        with YoutubeDL(options) as ydl:
+            info = ydl.extract_info(url, download=True)
+            return info
+Add YouTube downloader
