da7beecbb23a9126fd23cb44680e33f4f4e9d95d
mimpactlabs <mimpactmedia@gmail.com>
2026-07-13 22:03:03 +0700

    Update service.py

--- src/collector/service.py
+++ src/collector/service.py
@@ -1,1 +1,17 @@
+from .validator import URLValidator
+from .downloader import Downloader
+from .exceptions import InvalidURLException
 
+
+class CollectorService:
+
+    def __init__(self):
+        self.downloader = Downloader()
+
+    def collect(self, url: str):
+
+        if not URLValidator.is_valid(url):
+            raise InvalidURLException("Invalid YouTube URL")
+
+        return self.downloader.download(url)
+Add collector service
