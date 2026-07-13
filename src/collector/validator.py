c88a9a2e7e2006c67c9cf3ab172e6802665f77cb
mimpactlabs <mimpactmedia@gmail.com>
2026-07-13 21:56:25 +0700

    Add YouTube URL validator class

--- src/collector/validator.py
+++ src/collector/validator.py
@@ -1,1 +1,18 @@
+from urllib.parse import urlparse
 
+
+class URLValidator:
+    SUPPORTED_DOMAINS = (
+        "youtube.com",
+        "www.youtube.com",
+        "youtu.be",
+    )
+
+    @staticmethod
+    def is_valid(url: str) -> bool:
+        try:
+            parsed = urlparse(url)
+            return parsed.netloc in URLValidator.SUPPORTED_DOMAINS
+        except Exception:
+            return False
+Add YouTube URL validator
