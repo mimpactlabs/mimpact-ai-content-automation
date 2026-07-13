76414e56501b1461f66302407a52fe59aba74f96
mimpactlabs <mimpactmedia@gmail.com>
2026-07-13 22:00:29 +0700

    Update exceptions.py

--- src/collector/exceptions.py
+++ src/collector/exceptions.py
@@ -1,1 +1,7 @@
+class InvalidURLException(Exception):
+    """Raised when the URL is invalid."""
 
+
+class DownloadFailedException(Exception):
+    """Raised when video download fails."""
+Add collector exceptions
